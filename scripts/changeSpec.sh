3e273497f580cb1dbcbb6206fcc4fcdf1583578e
junhui <junhuitee@hotmail.com>
2021-06-07 23:55:29 +0800

    check for running instnace

--- scripts/changeSpec.sh
+++ scripts/changeSpec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-instanceId=`aws ec2 describe-instances --filter "Name=tag:restartOnPush,Values=true" --query "Reservations[*].Instances[*].{Instance:InstanceId}" --output text`
+instanceId=`aws ec2 describe-instances --filter "Name=tag:restartOnPush,Values=true" "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].{Instance:InstanceId}" --output text`
 echo $instanceId
 aws ec2 modify-instance-credit-specification --instance-credit-specification "InstanceId=$instanceId,CpuCredits=standard"
