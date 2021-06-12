f4aa3b3e18a91541f8ee15a68465650e584212cb
ftt.junhui.tee <ftt.junhui.tee@grabtaxi.com>
2021-06-12 14:56:13 +0800

    should work now

--- scripts/ci_register_task.sh
+++ scripts/ci_register_task.sh
@@ -2,16 +2,24 @@
 
 register_task(){
     SERVICE=$1
+    IMAGE_TAG="notmaster"
     NEW_IMAGE_URL="${ECR_REGISTRY}/${SERVICE}:${IMAGE_TAG}"
-    aws ecs describe-task-definition --task-definition $SERVICE --query 'taskDefinition.{containerDefinitions:containerDefinitions}' > task-definitions.json 
-    jq '.containerDefinitions[0].image="'"$NEW_IMAGE_URL"'"' task-definitions.json
 
-    aws ecs register-task-definition \
-        --family $SERVICE \
-        --cli-input-json file://task-definitions.json \
-        --requires-compatibilities FARGATE \
-        --network-mode awsvpc \
-        --cpu 256 --memory 512 
+    aws ecs describe-task-definition --task-definition $SERVICE \
+        --query '{  containerDefinitions: taskDefinition.containerDefinitions,
+                    family: taskDefinition.family,
+                    taskRoleArn: taskDefinition.taskRoleArn,
+                    executionRoleArn: taskDefinition.executionRoleArn,
+                    networkMode: taskDefinition.networkMode,
+                    volumes: taskDefinition.volumes,
+                    placementConstraints: taskDefinition.placementConstraints,
+                    requiresCompatibilities: taskDefinition.requiresCompatibilities,
+                    cpu: taskDefinition.cpu,
+                    memory: taskDefinition.memory}' > task-definitions.json 
+    
+    jq '.containerDefinitions[0].image="'"$NEW_IMAGE_URL"'"' task-definitions.json > tmp.$$.json && mv tmp.$$.json task-definitions.json
+
+    aws ecs register-task-definition  --family $SERVICE --cli-input-json "$NEW_TASK_DEFINTION"
 }
 
 register_task $1
